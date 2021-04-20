8b11ef56de53324541b02701668ca73f8a34db7f
rudrvij <rudrapaka@gmail.com>
2021-04-19 21:56:47 -0500

    sadfsdf

--- Grandslam.Service/TournamentRepository.cs
+++ Grandslam.Service/TournamentRepository.cs
@@ -11,7 +11,7 @@ namespace Grandslam.Service
         {
             return new List<Tournament>()
             {
-                new Tournament() { Name="Australian Open", Stadium ="Rod Laver Arena", Country ="Australia" },
+                new Tournament() { Name="Australian Open1", Stadium ="Rod Laver Arena", Country ="Australia" },
                 new Tournament() { Name = "French open", Stadium ="Stade Roland Garros", Country ="France"},
                 new Tournament() { Name ="Wimbledon", Stadium ="Wimbledon", Country = "United Kingdom" },
                 new Tournament() { Name="US Open", Stadium="BJK National Tennis Center", Country="USA"},
